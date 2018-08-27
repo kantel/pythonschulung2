0007a6e0032f87ad40b05e301b7358443ba4fdd6
Jörg Kantel <joerg@kantel.de>
2018-08-27 16:31:20 +0200

    Stage 2.1

--- sources/stage01/hallo.py
+++ sources/stage01/hallo.py
@@ -1,1 +1,11 @@
-print("Hallo Welt!")
+my_supernumber = 333
+
+def add_number(_my_supernumber):
+    my_supernumber = _my_supernumber
+    print(my_supernumber)
+    my_supernumber += my_supernumber
+    print("The number of the beast = " + str(my_supernumber))
+
+print(my_supernumber)
+add_number(my_supernumber)
+print(my_supernumber)
---
+++ sources/stage01/tt.py
@@ -0,0 +1,12 @@
+import turtle as t
+
+wn = t.Screen()
+wn.colormode(255)
+wn.bgcolor(43, 62, 80)
+wn.setup(width = 600, height = 600)
+wn.title("Ein Super-Duper Turtle-Programm")
+
+alex = t.Turtle()
+alex.shape("turtle")
+# Hier kommt jetzt Euer Programm-Code hin
+wn.mainloop()
