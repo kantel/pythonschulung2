b13fb24770dd7789f3845aeb147a720dff272951
Jörg Kantel <joerg@kantel.de>
2020-11-18 16:24:35 +0100

    Update stage09.py

--- sources/spaceinvaders/stage09.py
+++ sources/spaceinvaders/stage09.py
@@ -155,10 +155,10 @@ class Invader(Sprite):
 wn = t.Screen()
 wn.bgcolor("#000000")
 wn.bgcolor("#2b3e50")
-path_to_bg = os.path.join(os.getcwd(), "sources/spaceinvaders/farback.gif")
-wn.bgpic(path_to_bg)
-ufo = os.path.join(os.getcwd(), "sources/spaceinvaders/ufo.gif")
-# ufo = "ufo.gif"
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
+wn.bgpic("farback.gif")
+ufo = "ufo.gif"
 wn.register_shape(ufo)
 
 wn.setup(width = WIDTH, height = HEIGHT)
